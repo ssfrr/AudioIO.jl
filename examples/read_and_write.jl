b14cd371ce95f9db3bc7c725b7a92a1a58c5f53f
Bill <wherrera10@users.noreply.github.com>
2015-11-22 18:34:05 -1000

    Update read_and_write.jl

--- examples/read_and_write.jl
+++ examples/read_and_write.jl
@@ -10,8 +10,8 @@ CHANNELS = 2
 SRATE = 44100
 RECORD_SECONDS = 3
 
-BUFFER = zeros(Int16, SRATE * RECORD_SECONDS)
-BUFSIZE = length(BUFFER)
+BUFFER = zeros(Int16, SRATE * RECORD_SECONDS * 4)
+BUFSIZE = SRATE * RECORD_SECONDS
 
 """
 choose the devices for 2 channel IO
@@ -21,7 +21,8 @@ function choose_input_output()
     indev = -1
     outdev = -1
     for aud in devices
-        if (aud.max_input_channels == CHANNELS) & (indev == -1)
+        println("$(aud.device_index) $(aud.name)")
+        if (aud.max_input_channels == CHANNELS) & (indev == -1 )
             indev = aud.device_index
         end
         if(aud.max_output_channels == CHANNELS) & (outdev == -1)
@@ -44,30 +45,34 @@ end
 """
 read from input
 """
-function read_blocking(devnum)
+function read_blocking(devnum, buffer)
     instream = AudioIO.open_read(devnum, CHANNELS, SRATE, CHUNKSIZE)
-    BUFFER = AudioIO.read(instream, BUFSIZE)
+    buf = AudioIO.read(instream, BUFSIZE)
+    buflen = length(buf)
+    buffer[1: buflen] = buf[1: buflen]
     AudioIO.Pa_CloseStream(instream.stream)
 end
 
 """
 write to output
 """
-function write_blocking(devnum)
+function write_blocking(devnum, buffer)
     outstream = AudioIO.open_write(devnum, CHANNELS, SRATE, CHUNKSIZE)
-    AudioIO.write(outstream, BUFFER)
+    AudioIO.write(outstream, buffer)
     AudioIO.Pa_CloseStream(outstream.stream)
 end
 
+read_position = 0
+write_position = 0
 """
 read callback function
 """
-read_position = 0
-write_position = 0
 function rcallback(buf)
+    println("In read callback")
+    global read_position
     read_size = length(buf)
-    if read_position + read_size > length(buffer)
-        read_size = length(buffer) - read_position
+    if read_position + read_size > length(BUFFER)
+        read_size = length(BUFFER) - read_position
     end
     if read_size > 1
         BUFFER[read_position + 1 : read_position + read_size] = 
@@ -80,10 +85,12 @@ end
 """
 write callback function
 """
-function wcallback(buf)
+function wcallback(buffer)
+    println("In write callback")
+    global write_position
     write_size = CHUNKSIZE * CHANNELS
-    if write_position + write_size > length(buffer)
-        write_size = length(buffer) - write_position
+    if write_position + write_size > length(BUFFER)
+        write_size = length(BUFFER) - write_position
     end
     if write_size < 2
        return Void
@@ -91,6 +98,7 @@ function wcallback(buf)
     start_position = write_position + 1
     write_position += write_size
     buf = BUFFER[start_position: write_position]
+    buf
 end
 
 """
@@ -111,7 +119,7 @@ function start_write_callback(devnum)
                        AudioIO.make_c_callback(wcallback, FORMAT))
 end
 
-function play_note(frequency, amplitude, duration, srate, ostream)
+function make_note_buffer(frequency, amplitude, duration, srate)
     N = round(Int, srate / frequency)
     T = round(Int, frequency * duration)  # repeat for T cycles
     dt = 1.0 / srate
@@ -123,23 +131,29 @@ function play_note(frequency, amplitude, duration, srate, ostream)
         tone[idx + 1] = tone[idx]
         idx += 2
     end
-    AudioIO.write(ostream, tone)
+    tone
+end
+
+function play_note(frequency, amplitude, duration, srate, ostream)
+    note = make_note_buffer(frequency, amplitude, duration, srate)
+    AudioIO.write(ostream, note)
 end
 
 INS, OUTS = choose_input_output()
 
-read_blocking(INS)
+read_blocking(INS, BUFFER)
 println("Finished blocking type reading device number $INS")
+println("Recording volume is $(mean(abs(BUFFER))*(100/32767))% of max")
 sleep(3)
-write_blocking(OUTS)
+write_blocking(OUTS, BUFFER)
 println("Finished blocking type writing device number $OUTS")
 
 start_read_callback(INS)
 println("Started callback type reading device number $INS")
-sleep(3)
+
+BUFFER = make_note_buffer(88.0, 0.4, 3, SRATE)
 ostream = start_write_callback(OUTS)
 println("Started callback type writing device number $OUTS")
-sleep(3)
 AudioIO.Pa_CloseStream(ostream.stream)
 
 outstream = AudioIO.open_write(OUTS, CHANNELS, SRATE, CHUNKSIZE)
@@ -147,10 +161,10 @@ outstream = AudioIO.open_write(OUTS, CHANNELS, SRATE, CHUNKSIZE)
 # play the C major scale
 scale = [130.8, 146.8, 164.8, 174.6, 195.0, 220.0, 246.9, 261.6]
 for note in scale
-    play_note(note, 0.5, 0.75, SRATE, outstream)
+    play_note(note, 0.1, 0.75, SRATE, outstream)
 end
 
 # up an octave
 for note in scale[2:8]
-    play_note(2*note, 0.5, 0.75, SRATE, outstream)
+    play_note(2*note, 0.1, 0.75, SRATE, outstream)
 end
