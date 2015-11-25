19447767e6321fafdb9e6f8c686b610affaf4cd6
Bill <wherrera10@users.noreply.github.com>
2015-11-24 21:58:16 -1000

    Update callback_read_write.jl

--- examples/callback_read_write.jl
+++ examples/callback_read_write.jl
@@ -45,17 +45,9 @@ end
 
 read_position = 0
 """
-Custom callback for read
-Must process frameCount frames from the input buffer
-Return 0 to ask for more data, 1 to ask for no more callbacks
+Custom callback for read, processes passed data array
 """
-function store_read(input::Ptr{Void}, output::Ptr{Void}, 
-                  frameCount::Culong, 
-                  timeInfo::Ptr{AudioIO.CCallbackTimeInfo}, 
-                  sflags::Culong, udata::Ptr{Void})
-    retval = 0
-    buf = pointer_to_array(Ptr{CALLBACK_DATATYPE}(input), 
-                           (frameCount * CHANNELS, ))
+function store_read(buf)
     read_size = length(buf)
     global read_position
     if read_position + read_size > length(BUFFER)
@@ -65,24 +57,15 @@ function store_read(input::Ptr{Void}, output::Ptr{Void},
         BUFFER[read_position + 1 : read_position + read_size] = 
             buf[1: read_size]
         read_position += read_size
-    else
-        retval = 1
     end
-    Cint(retval)
 end
 
 write_position = 0
 """
-Custom callback for write
-Must write frameCount frames to the output buffer
-Return 0 to continue to write data, 1 to ask for no more callbacks
+Custom callback for write, returns data array
 """
-function get_writeable(input::Ptr{Void}, output::Ptr{Void}, 
-                  frameCount::Culong, 
-                  timeInfo::Ptr{AudioIO.CCallbackTimeInfo}, 
-                  sflags::Culong, udata::Ptr{Void})
-    retval = 0
-    write_size = frameCount * CHANNELS
+function get_writeable()
+    write_size = CHUNKSIZE * CHANNELS
     global write_position
     if write_position + write_size > length(BUFFER)
         write_position = length(BUFFER) - write_size
@@ -91,45 +74,19 @@ function get_writeable(input::Ptr{Void}, output::Ptr{Void},
     start_position = write_position + 1
     write_position += write_size
     buf = BUFFER[start_position: write_position]
-    ccall(:memcpy, Ptr{Void}, (Ptr{Void}, Ptr{Void}, Cint), 
-          output, buf, length(buf) * sizeof(buf[1]))
-    Cint(retval)   
-end
-
-const rcallback = cfunction(store_read, Cint, (Ptr{Void}, 
-                               Ptr{Void}, 
-                               Culong, Ptr{AudioIO.CCallbackTimeInfo}, 
-                               Culong, Ptr{Void}))
-
-const wcallback = cfunction(get_writeable, Cint, (Ptr{Void}, 
-                               Ptr{Void}, 
-                               Culong, Ptr{AudioIO.CCallbackTimeInfo}, 
-                               Culong, Ptr{Void}))
-
-"""
-Start read using callback
-"""
-function start_read_callback(devnum)
-    rstream = AudioIO.open_read(devnum, CHANNELS, SRATE, CHUNKSIZE,
-                                false, FORMAT, rcallback)
-end
-
-"""
-Start write using callback
-"""
-function start_write_callback(devnum)
-    wstream = AudioIO.open_write(devnum, CHANNELS, SRATE, CHUNKSIZE, 
-                                 false, FORMAT, wcallback)
+    buf
 end
 
 INS, OUTS = choose_input_output()
 
-istream = start_read_callback(INS)
+istream = AudioIO.open_read(INS, CHANNELS, SRATE, CHUNKSIZE,
+                            false, FORMAT, store_read)
 println("Started callback type reading device number $INS")
 sleep(5)
 AudioIO.Pa_CloseStream(istream.stream)
 
-ostream = start_write_callback(OUTS)
+ostream = AudioIO.open_write(OUTS, CHANNELS, SRATE, CHUNKSIZE, 
+                             false, FORMAT, get_writeable)
 println("Started callback type writing device number $OUTS")
 sleep(3)
 AudioIO.Pa_CloseStream(ostream.stream)
