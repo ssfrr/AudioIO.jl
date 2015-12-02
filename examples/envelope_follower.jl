8eaea77625ff3d1f8fdd6fc9013fd05e17eff595
Robert Luke <robert.g.luke@gmail.com>
2015-12-02 23:59:15 +0100

    Allow changing input of Envelope node

--- examples/envelope_follower.jl
+++ examples/envelope_follower.jl
@@ -6,12 +6,13 @@ using DSP, AudioIO
 #
 
 type EnvelopeFollower <: AudioIO.AudioRenderer
+    in1::AudioIO.AudioNode
     a::Array
     b::Array
     x_last::Array
     y_last::Array
 
-    function EnvelopeFollower(cutOff, fs)
+    function EnvelopeFollower(cutOff, fs, in1=AudioInput())
 
         responsetype = Lowpass(cutOff; fs=fs)
         designmethod = Butterworth(2)
@@ -19,21 +20,22 @@ type EnvelopeFollower <: AudioIO.AudioRenderer
         f = DSP.PolynomialRatio(f)
         a = coefa(f)
         b = coefb(f)
-        new(a, b, zeros(a), zeros(b))
+        new(in1, a, b, zeros(a), zeros(b))
     end
 end
 
-typealias EnvelopeNode AudioIO.AudioNode{EnvelopeFollower}
+typealias Envelope AudioIO.AudioNode{EnvelopeFollower}
 
 import AudioIO.render
 function render(node::EnvelopeFollower, device_input::AudioIO.AudioBuf, info::AudioIO.DeviceInfo)
 
     # Low pass filter on absolute value of measurement
-    y = zeros(device_input)
+    input = render(node.in1, device_input, info)
+    y = zeros(input)
 
     for i in 1:length(y)
 
-        y[i] =  (abs(device_input[i]) * node.b[1]) +
+        y[i] =  (abs(input[i]) * node.b[1]) +
                 (node.x_last[1] *  node.b[2]) +
                 (node.x_last[2] *  node.b[3]) +
                 (node.y_last[1] * -node.a[2]) +
@@ -41,7 +43,7 @@ function render(node::EnvelopeFollower, device_input::AudioIO.AudioBuf, info::Au
 
         node.x_last[2] = node.x_last[1]
         node.y_last[2] = node.y_last[1]
-        node.x_last[1] = abs(device_input[i])
+        node.x_last[1] = abs(input[i])
         node.y_last[1] = y[i]
     end
 
@@ -63,7 +65,7 @@ type EnvelopePlotter <: AudioIO.AudioRenderer
     EnvelopePlotter() = new(AudioInput(), 500, 150)
 end
 
-typealias PlotterNode AudioIO.AudioNode{EnvelopePlotter}
+typealias Plot AudioIO.AudioNode{EnvelopePlotter}
 
 function render(node::EnvelopePlotter, device_input::AudioIO.AudioBuf, info::AudioIO.DeviceInfo)
 
@@ -87,7 +89,14 @@ end
 #
 
 
-p = PlotterNode(EnvelopeNode(5, 44100))
+p = Plot(Envelope(5, 44100, AudioInput()))
+play(p)
+sleep(0.00001)
+@printf("\n\nFollow Envelope\n")
+sleep(10)
+stop(p)
+
+p = Plot(Envelope(5, 44100))
 play(p)
 sleep(0.00001)
 @printf("\n\nFollow Envelope\n")
@@ -95,7 +104,7 @@ sleep(10)
 stop(p)
 
 @printf("\n\nFollow Absolute Signal\n")
-p = PlotterNode()
+p = Plot()
 play(p)
 sleep(10)
 stop(p)
